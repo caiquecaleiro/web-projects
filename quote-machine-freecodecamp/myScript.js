75811918b9b541ef41e9d34b8cdb68eab7759350
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-14 09:02:12 +0530

    Add getRandomQuote method, which returns a random quote from the Quotes on design API

--- quote-machine-freecodecamp/myScript.js
+++ quote-machine-freecodecamp/myScript.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  $('#quote-button').on('click', function(e) {
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(quote) {
-      console.log(quote);
-    });
-  });
+  getRandomQuote();
+  $('#quote-button').on('click', getRandomQuote);
+
+  function getRandomQuote() {
+    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(quote) {
+    // Clean the text and add a space char
+    $('#quote').text(' ');
+    // Add the quote text
+    $('#quote').append(quote[0].content);
+    // Clean the author text
+    $('#author').text('');
+    // Add the author's name
+    $('#author').append(quote[0].title);
+    // To remove the paragraph tag that comes with the content
+    $('#quote').text($('#quote').text());
+   });
+  }
 });
